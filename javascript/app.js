cfcca51209be42b64656e618b4b842d00c492b13
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-14 17:46:06 +0900

    배열 기본

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+const days0fWeek = ["mon", "tue" , "wed" , "thu" , "fri" , "sat"];
+
+// Get Item from Array
+console.log(days0fWeek);
+
+// Add one more day to the array
+days0fWeek.push("sun");
+
+console.log(days0fWeek);
