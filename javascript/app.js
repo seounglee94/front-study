60b28a3c52723626264a8fc15de331a6ef60794e
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-16 15:08:44 +0900

    caculaeter

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,25 @@
-const days0fWeek = ["mon", "tue" , "wed" , "thu" , "fri" , "sat"];
+const caculater = {
+    add : function(a, b) {
+        console.log(a + b)
+    },
+    minus : function(a, b) {
+        console.log(a - b)
+    },
+    multi : function(a, b) {
+        console.log(a * b)
+    },
+    div : function(a, b) {
+        console.log(a / b)
+    },
+    power :function(a, b) {
+        console.log(a ** b)
+    }
+}
 
-// Get Item from Array
-console.log(days0fWeek);
 
-// Add one more day to the array
-days0fWeek.push("sun");
+caculater.add(1, 1);
+caculater.minus(19 , 2);
+caculater.multi(2, 8);
+caculater.div(8, 4);
+caculater.power(2, 2);
 
-console.log(days0fWeek);
