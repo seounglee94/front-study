13168b1c4128bc39b20c54f9a8bbf36d38dca5c6
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-17 20:16:09 +0900

    조건문 condition

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,8 @@
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
+if(isNaN(age)){
+    // condition == true
+    console.log("Please write a number"); 
+} else {
+    console.log("Thank you for writing your age.");
+}
