f2a3a97fc34bc77f5c762fef1fc72bbac866a740
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-17 11:53:27 +0900

    parseInt를 사용하여 string을 숫자로 변환

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,3 @@
-const caculater = {
-    plus : function(a, b) {
-        return(a + b)
-    },
-    minus : function(a, b) {
-        return(a - b)
-    },
-    times : function(a, b) {
-        return(a * b)
-    },
-    divide : function(a, b) {
-        return(a / b)
-    },
-    power :function(a, b) {
-        return(a ** b)
-    }
-}
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = caculater.plus(5, 2);
-const minusResult = caculater.minus(plusResult, 5);
-const timeResult = caculater.times(10, minusResult);
-const divideResult = caculater.divide(timeResult, plusResult);
-const powerResult = caculater.power(divideResult, minusResult);
-
-
+console.log(age);
