89922f0957584f7068c2cfb3f0f5b9b4ce8e11de
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-19 21:51:29 +0900

    event 2

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,22 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick(){
-    console.log("wow!")
+    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
-
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 
 
 
