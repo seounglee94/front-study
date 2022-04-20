50df491a9d077abdb29bcd7f52c1165d87f64ff6
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-20 12:32:22 +0900

    Add files via upload

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,16 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1);
-
 function handleTitleClick(){
-    h1.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else{
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
+h1.addEventListener("click", handleTitleClick);
 
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD!");
-}
-
-h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
 
