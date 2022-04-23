1a5f702ea891b4ac4644891a3e58bd88f2ce5ab0
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-23 15:31:28 +0900

    Add files via upload

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,31 @@
 const loginForm = document.querySelector("#login-form");
 const longinInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSumit(event){
     event.preventDefault();
-    console.log(longinInput);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = longinInput.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    // greeting.innerText = "Hello " + username;
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSumit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSumit);
+} else {
+    paintGreetings(savedUsername);
+}
