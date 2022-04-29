001ab0022be8f1a57d5b9b8ac4d4b678b8b1f9a2
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-04-29 22:39:56 +0900

    Add files via upload

--- javascript/js/todo.js
+++ javascript/js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(){
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +31,21 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
+
+}
+
+function sayHello(item){
+    console.log("hello", item);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(sayHello);
+}
