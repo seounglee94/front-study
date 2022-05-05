1aaa7c24585af1cbcb4e15c95f38912b3040075c
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-05-05 23:10:11 +0900

    Add files via upload

--- javascript/js/weather.js
+++ javascript/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "952768a22eb8ff679a3bd8b2dc03d8d8"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+                const weather = document.querySelector("#weather span:first-child")
+                const city = document.querySelector("#weather span:last-child")
+            city.innerText = data.name;
+            weather.innerText =  `${data.weather[0].main} / ${data.main.temp}`;
+
+    })
 }
 
 function onGeoError(){
