82c472b9f6f96ace1e44e5d34150d767dbc3663d
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-05-14 16:05:17 +0900

    Add files via upload

--- javascript/복습/index.js
+++ javascript/복습/index.js
@@ -1,30 +1,56 @@
-const h1 = document.querySelector("h1");
 
-const handle ={
+const body = document.body;
 
-  en : function enter(){
-    h1.innerText = "Your mouse is enter!";
-    h1.style.color = "green";
-  },
+const big = "bigScreen";
+const medium = "mediumScreen";
+const small = "smallScreen";
 
-  le : function leave(){
-    h1.innerText = "Your mouse is leave!";
-    h1.style.color = "red";
-  },
-
-  ri : function rightClick(){
-    h1.innerText = "bye!";
-    h1.style.color = "blue";
-  },
-
-  re : function resized(){
-    h1.innerText = "resize!!!";
-    h1.style.color = "purple";
+function handle(){
+  let wi = window.innerWidth;
+  if(wi > 1000){
+    body.classList.add(big);
+    body.classList.remove(medium);
+  }else if(wi <= 1000 && wi > 700){
+    body.classList.add(medium);
+    body.classList.remove(big, small);
+  }else{
+    body.classList.add(small);
+    body.classList.remove(big, medium);
   }
-  
 }
 
-h1.addEventListener("mouseenter", handle.en);
-h1.addEventListener("mouseleave", handle.le);
-window.addEventListener("contextmenu", handle.ri);
-window.addEventListener("resize", handle.re);
+window.addEventListener("resize", handle);
+
+
+// const h1 = document.querySelector("h1");
+
+// const handle ={
+
+//   en : function enter(){
+//     h1.innerText = "Your mouse is enter!";
+//     h1.style.color = "green";
+//   },
+
+//   le : function leave(){
+//     h1.innerText = "Your mouse is leave!";
+//     h1.style.color = "red";
+//   },
+
+//   ri : function rightClick(){
+//     h1.innerText = "bye!";
+//     h1.style.color = "blue";
+//   },
+
+//   re : function resized(){
+//     h1.innerText = "resize!!!";
+//     h1.style.color = "purple";
+//   }
+  
+// }
+
+// h1.addEventListener("mouseenter", handle.en);
+// h1.addEventListener("mouseleave", handle.le);
+// window.addEventListener("contextmenu", handle.ri);
+// window.addEventListener("resize", handle.re);
+
+
