974f63301de137b4ac979626bd1906a974e92517
seounglee94 <83061014+seounglee94@users.noreply.github.com>
2022-05-14 15:49:02 +0900

    Add files via upload

--- javascript/복습/index.js
+++ javascript/복습/index.js
@@ -1,81 +1,30 @@
-// const ocean = document.querySelector("#ocean");
-// const red = document.querySelector("#red");
-// const superEventHandler = {
-//     en : function enter(){
-//         red.innerText = "The mouse is here";
-//         red.style.color = "red";
-//     },
+const h1 = document.querySelector("h1");
 
-//     le : function leave(){
-//         red.innerText = "The mouse is gone!";
-//         red.style.color = "green";
-//     },
+const handle ={
 
-//     ri : function rightClick(){
-//         ocean.innerText = "That was right click!";
-//         ocean.style.color = "blue";
-//     },
+  en : function enter(){
+    h1.innerText = "Your mouse is enter!";
+    h1.style.color = "green";
+  },
 
-//     si : function size(){
-//         red.innerText = "You just resized!";
-//         red.style.color = "purple";
-//     }
-// };
-// red.addEventListener("mouseenter", superEventHandler.en);
-// red.addEventListener("mouseleave", superEventHandler.le);
-// ocean.addEventListener("contextmenu", superEventHandler.ri);
+  le : function leave(){
+    h1.innerText = "Your mouse is leave!";
+    h1.style.color = "red";
+  },
 
-// window.addEventListener("resize", superEventHandler.si);
+  ri : function rightClick(){
+    h1.innerText = "bye!";
+    h1.style.color = "blue";
+  },
 
-
-// const ocean = document.querySelector("body");
-// const red = document.querySelector("h2");
-
-// const superEventHandler = {
-//   en: function enter() {
-//     red.innerText = "The mouse is here";
-//     red.style.color = "red";
-//   },
-
-//   le: function leave() {
-//     red.innerText = "The mouse is gone!";
-//     red.style.color = "green";
-//   },
-
-//   ri: function rightClick() {
-//     ocean.innerText = "That was right click!";
-//     ocean.style.color = "blue";
-//   },
-
-//   si: function size() {
-//     red.innerText = "You just resized!";
-//     red.style.color = "purple";
-//   }
-// };
-
-// red.addEventListener("mouseenter", superEventHandler.en);
-// red.addEventListener("mouseleave", superEventHandler.le);
-// ocean.addEventListener("contextmenu", superEventHandler.ri);
-
-// window.addEventListener("resize", superEventHandler.si);
-
-const hi = document.querySelector("body");
-const colors = ["#1abc9c", "#3498db", "#9b59b6"];
-
-function one() {
-  hi.style.color = "white";
-  const size = window.innerWidth;
-  console.log(size);
-  if(size <= 500){
-    hi.style.backgroundColor = colors[0];
-    console.log(size);
-  }else if(size > 700 && size <= 900){
-    hi.style.backgroundColor = colors[1];
-    console.log(size);
-  }else if(size > 900 && size <= 1100){
-    hi.style.backgroundColor = colors[2];
-    console.log(size);
+  re : function resized(){
+    h1.innerText = "resize!!!";
+    h1.style.color = "purple";
   }
+  
 }
 
-window.addEventListener("resize", one);
+h1.addEventListener("mouseenter", handle.en);
+h1.addEventListener("mouseleave", handle.le);
+window.addEventListener("contextmenu", handle.ri);
+window.addEventListener("resize", handle.re);
